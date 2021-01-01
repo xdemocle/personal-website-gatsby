b26f8a411f54d7454b30182f1685e3fd9a97569e
xdemocle <xdemocle@gmail.com>
2021-01-01 20:54:20 +0200

    adding styles and gatsby p[lugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,16 @@ module.exports = {
         trackingId: "UA-63832089-1",
       },
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
     "gatsby-plugin-sharp",
+    "gatsby-plugin-breakpoints",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-offline",
     {
